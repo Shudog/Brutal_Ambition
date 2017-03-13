52c7a5fcbf549cc54e1ae0943d584c3be27f14ef
Shumaker <NS12379@kroger.com>
2017-03-13 13:05:57 -0400

    textfield now works on enter for newplayerform

--- src/newPlayerForm.java
+++ src/newPlayerForm.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 class newPlayerForm extends JPanel {
     private JTextField field = new JTextField(10);
@@ -13,6 +15,14 @@ class newPlayerForm extends JPanel {
                 okButtonAction();
             }
         });
+
+        field.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                okButtonAction();
+            }
+        });
+
         add(field);
         add(okButton);
     }
